22927da09df3ff14c3eb3c4ccdc0bd31c5c3cbaf
Glenn Kirchner <greywolf359@gmail.com>
2017-05-08 19:00:50 -0700

    initialize repo

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+
+const PORT = 3000;
+
+//middleware example
+var middleware = {
+	requireAuthentication: function(req,res,next){
+		console.log('private route hit');
+		next();
+	},
+	logger: function(req,res,next){
+		var d = new Date().toString();
+		console.log(d + ' REQUEST: ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+}
+
+app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, (req,res)=>{
+	res.send('about');
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.listen(PORT,()=>{
+	console.log('listening on port: ' + PORT);
+});
